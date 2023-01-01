b41138ab27f97c19277df9452af43a0e451b5707
Dannie Myers <dmyers19@woh.rr.com>
2022-12-31 19:15:49 -0500

    Added FormsModule

--- AngularPartII/Chapter 14 Using Events and Forms/eventsforms/src/app/app.module.ts
+++ AngularPartII/Chapter 14 Using Events and Forms/eventsforms/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 // import { AppComponent } from './app.component';
 import { ProductComponent } from "./component";
@@ -9,7 +10,7 @@ import { ProductComponent } from "./component";
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [ProductComponent]
