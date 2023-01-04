2631b31d510f3ba0914208e1f9f96851bab7215b
Dannie Myers <dmyers19@woh.rr.com>
2023-01-04 15:06:05 -0500

    getValidationMessage added

--- AngularPartII/Chapter 14 Using Events and Forms/eventsforms/src/app/component.ts
+++ AngularPartII/Chapter 14 Using Events and Forms/eventsforms/src/app/component.ts
@@ -27,4 +27,28 @@ export class ProductComponent {
     addProduct(p: Product) {
         console.log("New Product: " + this.jsonProduct);
     }
+
+    getValidationMessages(state: any, thingName?: string) {
+        let thing: string = state.path || thingName;
+        let messages: string[] = [];
+        if (state.errors) {
+            for (let errorName in state.errors) {
+                switch (errorName) {
+                    case "required":
+                        messages.push(`You must enter a ${thing}`);
+                        break;
+                    case "minlength":
+                        messages.push(`A ${thing} must be at least
+                            ${state.errors['minlength'].requiredLength}
+                            characters`);
+                            break;
+                    case "pattern":
+                        messages.push(`The ${thing} contains
+                            illegal characters`);
+                        break;
+                    }
+                }
+            }
+            return messages;
+    }
 }
