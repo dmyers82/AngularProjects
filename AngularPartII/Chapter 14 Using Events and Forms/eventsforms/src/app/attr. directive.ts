83abc11c766277018f5c2209318c0f77d05fc8af
Dannie Myers <dmyers19@woh.rr.com>
2023-01-23 16:58:39 -0500

    attribute directive changes efined

--- AngularPartII/Chapter 14 Using Events and Forms/eventsforms/src/app/attr. directive.ts
+++ AngularPartII/Chapter 14 Using Events and Forms/eventsforms/src/app/attr. directive.ts
@@ -1,12 +1,19 @@
-import { Directive, ElementRef, Attribute } from "@angular/core";
+import { Directive, ElementRef, Attribute, Input } from "@angular/core";
 
 @Directive({
     selector: "[pa-attr]",
 })
 
 export class PaAttrDirective {
-    constructor(element: ElementRef, @Attribute("pa-attr-class") bgClass: string) {
-        element.nativeElement.classList.add(bgClass || "bg-success", "text-white");
+    constructor(private element: ElementRef) {
         console.log("element.nativeElement.classList - " + element.nativeElement.OuterHTML);
+    }
+
+    @Input("pa-attr")
+    bgClass: string;
+    
+    ngOnInit() {
+        this.element.nativeElement.classList.add(this.bgClass || "bg-success",
+            "text-white");
     }
 }
