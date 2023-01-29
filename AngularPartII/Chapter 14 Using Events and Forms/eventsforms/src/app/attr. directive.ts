b0fea4f72df167bcb3ae6e8cd0051490f3d17af1
Dannie Myers <dmyers19@woh.rr.com>
2023-01-28 20:25:50 -0500

    Input Property Changes

--- AngularPartII/Chapter 14 Using Events and Forms/eventsforms/src/app/attr. directive.ts
+++ AngularPartII/Chapter 14 Using Events and Forms/eventsforms/src/app/attr. directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Attribute, Input } from "@angular/core";
+import { Directive, ElementRef, Attribute, Input, SimpleChange} from "@angular/core";
 
 @Directive({
     selector: "[pa-attr]",
@@ -11,9 +11,22 @@ export class PaAttrDirective {
 
     @Input("pa-attr")
     bgClass: string;
-    
+
     ngOnInit() {
+        console.log("ngOnInit called");
         this.element.nativeElement.classList.add(this.bgClass || "bg-success",
             "text-white");
+    }
+
+    ngOnChanges(changes: {[property: string]: SimpleChange }) {
+        console.log("ngOnChanges called");
+        let change = changes["bgClass"];
+        let classList = this.element.nativeElement.classList;
+        if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+            classList.remove(change.previousValue);
+        }
+        if (!classList.contains(change.currentValue)) {
+            classList.add(change.currentValue);
+        }
     }
 }
