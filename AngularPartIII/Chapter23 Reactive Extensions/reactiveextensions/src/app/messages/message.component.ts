8245c1b5d0cfbc6581d5df34cbb8d6f3058e0c48
Dannie Myers <dmyers19@woh.rr.com>
2023-02-19 11:26:56 -0500

    added more reaactiveextension code

--- AngularPartIII/Chapter23 Reactive Extensions/reactiveextensions/src/app/messages/message.component.ts
+++ AngularPartIII/Chapter23 Reactive Extensions/reactiveextensions/src/app/messages/message.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MessageService } from "./message.service";
 import { Message } from "./message.model";
+import { Observable } from "rxjs";
 
 @Component({
     selector: "paMessages",
@@ -10,7 +11,8 @@ import { Message } from "./message.model";
 export class MessageComponent {
     lastMessage: Message;
     constructor(messageService: MessageService) {
-        messageService.registerMessageHandler(m => this.lastMessage = m);
+        messageService.messages.subscribe(m => this.lastMessage = m);
+        //messageService.registerMessageHandler(m => this.lastMessage = m);
         console.log("MessageComponent constructor called");
     }
 }
--- AngularPartIII/Chapter23 Reactive Extensions/reactiveextensions/src/app/messages/message.service.ts
+++ AngularPartIII/Chapter23 Reactive Extensions/reactiveextensions/src/app/messages/message.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from "@angular/core";
 import { Message } from "./message.model";
+import { Observable } from "rxjs";
+import { Subject } from "rxjs";
 
 @Injectable()
 
 export class MessageService {
+    private subject = new Subject<Message>();
     private handler: (m: Message) => void;
+
     reportMessage(msg: Message) {
-        if (this.handler != null) {
+        this.subject.next(msg);
+        console.log("reportMessage called");
+        /* if (this.handler != null) {
             this.handler(msg);
             console.log("reportMessage called");
-        }
+        } */
+    }
+
+    get messages(): Observable<Message> {
+        return this.subject;
     }
     
     registerMessageHandler(handler: (m: Message) => void) {
