4f54d3b56cc447b44a61f0cc8abae37352127500
Dannie Myers <dmyers19@woh.rr.com>
2023-06-30 00:33:53 -0400

    Initial Changes

--- AzureRouting/src/app/app.module.ts
+++ AzureRouting/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
+import { MainMenuComponent } from './mainmenu.component'
 import { AppComponent } from './app.component';
+import { routing } from "./app.routing"
+import { SearchCustomerComponent } from './search/searchcustomer';
+import { NameListComponent } from './search/namelist.component';
+import { PersonalDemographComponent } from './customerinfo/personaldemographics/personaldemograph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent, CustomerFolderComponent, NameListComponent, 
+    PersonalDemographComponent, SearchCustomerComponent],
   imports: [
-    BrowserModule
+    BrowserModule, routing
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainMenuComponent]
+  // bootstrap: [AppComponent]
 })
 export class AppModule { }
